5e6834433b1687f9a5f52a3981c44998d382b939
Pedro Costa Burilli <pecoburilli@gmail.com>
2026-09-02 12:23:47 -0300

    Middleware.js ajustado
    Adicionando novas rotas para o middleware.js trabalhar e ajustando a lógica

--- letterbox-webapp/src/middleware.js
+++ letterbox-webapp/src/middleware.js
@@ -2,22 +2,25 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request) {
     // Captura a string do token
-    const token = request.cookies.get('token_usuario')?.value; // obter token
-
+    const token = request.cookies.get('session')?.value; // obter token
     const { pathname } = request.nextUrl; // rota do usuario
 
     if (pathname === '/login' && token) {
-        return NextResponse.redirect(new URL('/', request.url));
+        return NextResponse.redirect(new URL('/perfil', request.url));
     } // Se acessar /login com token redireciona para /menu (TESTANDO)
 
     if (pathname === '/register' && token) {
-        return NextResponse.redirect(new URL('/', request.url));
+        return NextResponse.redirect(new URL('/perfil', request.url));
     } // Se acessar /login com token redireciona para /menu (TESTANDO)
+
+    if (pathname === '/perfil' && token === undefined) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    } // Se acessar /perfil sem token redireciona para /login (TESTANDO)
     
     return NextResponse.next();
 }
 
 // Configuração para interceptar as rotas
 export const config = {
-    matcher: ['/menu/:path*', '/login', '/register'],
+    matcher: ['/menu/:path*', '/login', '/register', '/perfil', '/admin'],
 };
