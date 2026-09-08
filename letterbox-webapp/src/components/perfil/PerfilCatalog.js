e45e859634412acf4e59d782f5a5fd22e5cd1a10
Pedro Costa Burilli <pecoburilli@gmail.com>
2026-09-08 18:50:08 -0300

    Ajuste no front-end (BASE)
    Criado pequena lógica para, ao clicar em seu catalgo, abrir uma aba mostrando a respeito do jogo clicado.

--- letterbox-webapp/src/components/perfil/PerfilCatalog.js
+++ letterbox-webapp/src/components/perfil/PerfilCatalog.js
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 export default function PerfilCatalog() {
   const [catalogo, setCatalogo] = useState(null)
   const [carregando, setCarregando] = useState(true)
+  const [jogoSelecionado, setJogoSelecionado] = useState(null)
 
   useEffect(() => {
     ObterCatalogo();
@@ -37,9 +38,24 @@ export default function PerfilCatalog() {
       ) : (
         catalogo?.map((jogo) => (
           <div key={jogo?.id} className=''>
-            <img src={jogo?.img_url} className="w-full h-full object-cover rounded-md"/>
+            <img 
+              src={jogo?.img_url}
+              onClick={() => {setJogoSelecionado(jogo)}}
+              className="w-full h-full object-cover rounded-md"
+            />
           </div>
         ))
+      )}
+      {jogoSelecionado && (
+        <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 p-4'>
+          <div className='w-full max-w-md rounded-lg bg-gray-800 p-6 text-white shadow-xl'>
+            <div className="flex items-center justify-between">
+              <p className='capitalize'>{jogoSelecionado.name}</p>
+              <button onClick={() => {setJogoSelecionado(null)}}>X</button>
+            </div>
+            <p>lorem</p>
+          </div>
+        </div>
       )}
     </div>
   )
