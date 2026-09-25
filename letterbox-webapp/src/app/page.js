ca0a133b8de0a40395a9157ca6f13681447dd8fa
Pedro Costa Burilli <pecoburilli@gmail.com>
2026-09-25 17:46:55 -0300

    Ajuste na página inicial do site
    Página inicial visualmente melhorada:
    
    - Realizado ajustes de cor e designe
    - Adicionado capacidade de clicar na imagem, direcionado para o caminho do jogo.
    - Entre outros ajustes

--- letterbox-webapp/src/app/page.js
+++ letterbox-webapp/src/app/page.js
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
-export default function Page() {
+export default function Home() {
   const [jogos, setJogos] = useState([]);
   const [carregando, setCarregando] = useState(true);
   const router = useRouter();
@@ -59,30 +59,35 @@ export default function Page() {
   }
 
   return (
-    <div id="div_principal" className="w-full max-w-7xl mx-auto px-4 py-6">
+    <main className="home-page">
       <div className="flex flex-col items-center">
-        <h1 className="text-3xl md:text-4xl font-bold mb-6 text-center">
-          Lista de Jogos Cadastrados
-        </h1>
-
-        <div id="tabela_jogos" className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-4 w-full justify-items-center">
+        <div className="w-full h-80 md:h-128 bg-cover bg-center bg-no-repeat rounded-4xl flex flex-col justify-end pb-8 relative" style={{ backgroundImage: `url(${jogos?.[jogos?.length -1]?.['img_url']})`}}>
+          <div className="absolute inset-0 bg-linear-to-t from-primary-900 via-slate-950/1 justify-end pb-8 px-4"></div>
+          <div className="absolute inset-0 bg-linear-to-r from-primary-900 via-slate-950/1 justify-end pb-8 px-4"></div>
+          <div className="absolute inset-0 bg-linear-to-l from-primary-900 via-slate-950/1 justify-end pb-8 px-4"></div>
+          <h1 className="relative z-10 text-2xl md:text-3xl font-bold text-center text-white drop-shadow-lg px-4">
+            Avalie seus jogos preferidos.<br/>Salve aqueles que deseja jogar.<br/>Se divirta!
+          </h1>
+        </div>
+        <div id="tabela_jogos" className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-2 w-full justify-items-center p-2">
           {carregando ? (
             <p className="col-span-full text-center">Carregando jogos...</p>
           ) : (
             jogos?.map((jogo) => (
-              <div key={jogo?.id || jogo?.name} className="w-full flex flex-col items-center bg-slate-900 rounded-3xl p-2">
-                <p className="capitalize text-center text-sm font-medium">{jogo?.name}</p>
-                <img 
-                  src={jogo?.img_url} 
-                  alt={jogo?.name} 
-                  className="w-full h-48 object-cover rounded-t-3xl"
-                />
-                <button className='bg-emerald-700 hover:bg-emerald-500 w-full rounded-b-3xl' onClick={(e) => {AdicionarJogo(e, jogo)}}>Adicionar Jogo</button>
+              <div key={jogo?.id || jogo?.name} className="w-full flex flex-col items-center bg-primary-900 rounded-3xl">
+                <a href={`/games/${jogo?.name}`}>
+                  <img
+                    src={jogo?.img_url} 
+                    alt={jogo?.name} 
+                    className="w-full h-48 object-cover rounded-t-3xl"
+                  />
+                </a>
+                <button className='bg-primary-700 hover:bg-primary-500 w-full rounded-b-3xl' onClick={(e) => {AdicionarJogo(e, jogo)}}>Adicionar Jogo</button>
               </div>
             ))
           )}
         </div>
       </div>
-    </div>
+    </main>
   );
 };
